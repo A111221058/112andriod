7506faa7a42ca9a6abd8e5db40c3d7c2d78119e5
A111221058 <A111221058@mail.shu.edu.tw>
2024-05-30 18:03:23 +0800

    0530

---
+++ FireBaseFirstTry_0530/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "FirebaseFirstTry2024_0530"
+include(":app")
+ 
